d44b7cd727716283437ccf79a7f88049d9e34a16
Miriam Suzanne <miriam@oddbird.net>
2024-09-07 10:19:17 -0600

    Dont lazy-load the og hero facepalm

--- plugins/images.js
+++ plugins/images.js
@@ -5,17 +5,17 @@ export default function(eleventyConfig, options = {}) {
   const imgFolder = options.in;
   const opts = { ...options.out };
 
-  async function getImageData(src) {
+  async function getImageData(src, config) {
     const imgSrc = imgFolder && !src.includes('://')
       ? join(imgFolder, src)
       : src;
 
-    const metadata = await Image(imgSrc, opts);
+    const metadata = await Image(imgSrc, { ...opts, ...config});
     return metadata;
   }
 
-  async function imageSrc(src) {
-    const metadata = await getImageData(src);
+  async function imageSrc(src, config) {
+    const metadata = await getImageData(src, config);
     const img = metadata.jpeg[metadata.jpeg.length - 1];
     return img.url;
   }
