541a06c41db3db5c06cde1e8f69c12f8fe8f30ae
Miriam Suzanne <miriam@oddbird.net>
2026-05-22 00:39:28 -0600

    Open graph improvements, and hidden controls

--- plugins/markdown.js
+++ plugins/markdown.js
@@ -2,7 +2,6 @@ import markdownIt from 'markdown-it';
 import mdMark from 'markdown-it-mark';
 import mdDefList from 'markdown-it-deflist';
 import mdFootNote from 'markdown-it-footnote';
-import removeMd from 'remove-markdown';
 
 const mdIt = markdownIt({
   html: true,
@@ -14,7 +13,6 @@ const mdIt = markdownIt({
 
 const block = (content) => (content ? mdIt.render(content.trim()) : '');
 const inline = (content) => (content ? mdIt.renderInline(content.trim()) : '');
-const mdRemove = (content) => (content ? removeMd(content.trim()) : '');
 
 const splitString = '<!-- intro -->';
 const splitContent = (content) => {
@@ -29,7 +27,6 @@ const getContent = (content) => splitContent(content)[1];
 export default function (eleventyConfig) {
   eleventyConfig.addFilter('md', block);
   eleventyConfig.addFilter('mdI', inline);
-  eleventyConfig.addFilter('mdRemove', mdRemove);
 
   eleventyConfig.addGlobalData('splitString', splitString);
   eleventyConfig.addFilter('splitContent', splitContent);
--- src/src.11tydata.js
+++ src/src.11tydata.js
@@ -1,8 +1,49 @@
-const ogSrc = (data) => data.isOg ? null : `/_og${data.page.url}`;
+import removeMd from 'remove-markdown';
+
+const shorter = (string, max) => {
+  const dot = string.lastIndexOf('.');
+  const fade = string.lastIndexOf('…');
+
+  if (dot || fade) {
+    const end = Math.max(dot, fade);
+    const newString = string.substring(0, end);
+    return (newString <= max)
+      ? newString
+      : shorter(newString, max);
+  }
+
+  const newString = string.substring(0, max);
+  const space = newString.lastIndexOf(' ');
+  return `${newString.substring(0, space)}…`
+}
+
+const description = (data) => {
+  const max = 160;
+  if (data.description) return data.description;
+
+  if (data.summary) {
+    if (typeof data.summary === 'string') return removeMd(data.summary);
+    if (Array.isArray(data.summary)) return removeMd(data.summary[0]);
+  }
+
+  if (data.page.inputPath.endsWith('.md')) {
+    const raw = data.page.rawInput.split('<!-- intro -->')[0];
+    const pre = removeMd(raw);
+
+    if (pre.length <= max) return pre;
+    return shorter(pre, max);
+  }
+
+  if (typeof data.sub === 'string') return removeMd(data.sub);
+
+  return data.site.description;
+}
 
 export default {
   eleventyComputed: {
-    ogSrc,
+    baseURL: (data) => process.env.URL || `https://${data.site.domain}`,
     listOf: (data) => data.listOf || data.index,
+    title: (data) => data.title || data.site.title,
+    description,
   },
 };
